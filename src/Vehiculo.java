5e3d6e3d71181a48915b69e27d3cb2dad0e9a6b9
Natalia Cabrera Anaya <natalia.cabrera@est.iudigital.edu.co>
2025-05-18 17:50:36 -0500

    Se modificó la línea 55 agregando kilometraje. Se modificó la línea 61 agregando kilometraje. Se modificó la línea 62 agregando marca.

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -52,13 +52,13 @@ class Auto extends Vehiculo {
 class Camioneta extends Vehiculo {
     // Constructor de la clase Camioneta
     public Camioneta(String codigo, String marca, int modelo, double kilometraje) {
-        super(codigo, marca, "Camioneta", modelo, ***********);
+        super(codigo, marca, "Camioneta", modelo, kilometraje);
     }
 }
 
 class Motocicleta extends Vehiculo {
     // Constructor de la clase Motocicleta
-    public Motocicleta(String codigo, String marca, int modelo, double ********) {
-        super(codigo, ********, "Motocicleta", modelo, kilometraje);
+    public Motocicleta(String codigo, String marca, int modelo, double kilometraje) {
+        super(codigo, marca, "Motocicleta", modelo, kilometraje);
     }
 }
